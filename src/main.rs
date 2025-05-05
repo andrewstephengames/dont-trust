3cd49a8a9fa4aa5b6a22ce6dbd54ab71c95ed810
andrewstephenxyz <andrewstephen870@gmail.com>
2025-05-05 09:48:10 +0300

    Add grid and fix camera

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,16 @@ fn main() {
         .build();
     
     let mut camera= Camera3D::perspective(
-        Vector3 { x: 0.0, y: 0.0, z: 0.0 }, 
-        Vector3 { x: 1.0, y: 1.0, z: 0.0 },
-        Vector3 {x: 0.0, y: 0.0, z: 1.0},
-        70.0
+        Vector3 { x: 0.0, y: 5.0, z: 5.0 }, 
+        Vector3 { x: 0.0, y: 0.0, z: 0.0 },
+        Vector3 {x: 0.0, y: 1.0, z: 0.0},
+        45.0
     );
     rl.set_target_fps(FPS);
     let (x, y, z) = (5.0, 5.0, 5.0);
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::BLACK);
         let mouse = d.get_mouse_position();
         let key = d.get_key_pressed();
         window_x = d.get_render_width();
@@ -42,7 +43,11 @@ fn main() {
             _ => {}
         }
 
-        d.clear_background(Color::BLACK);
+        {
+            let mut d3= d.begin_mode3D(camera);
+            d3.draw_grid(10, 1.0);
+        }
+        //d.draw_g
         d.draw_text(&format!("{}, {}", window_x, window_y), 0, 0, 30, Color::LIME);
         d.draw_text(&format!("{}, {}, {}", camera.position.x, camera.position.y, camera.position.z), 0, 50, 30, Color::RED);
         d.draw_fps(0, 100);
