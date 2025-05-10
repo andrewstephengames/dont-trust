ffd4c8b92071efa86911fe157424e2a26cc82603
andrewstephenxyz <andrewstephen870@gmail.com>
2025-05-11 02:31:05 +0300

    Add terrain generation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::ffi::CString;
+use noise::{NoiseFn, Perlin, Seedable};
+use std::ptr;
+use std::mem;
 
 use raylib::{
-    core::{texture::*, math::*},
-    ffi::{DrawModel, LoadModel, DrawModelWires, GenImageGradientLinear, GenMeshCube, GenMeshCylinder, LoadModelFromMesh, LoadTextureFromImage, SetConfigFlags, SetMaterialTexture},
-    prelude::*,
-    consts::MaterialMapIndex::*
+    consts::MaterialMapIndex::*, core::{math::*, texture::*}, ffi::{DrawModel, DrawModelWires, GenImageGradientLinear, GenImagePerlinNoise, GenMeshCube, GenMeshCylinder, GenMeshHeightmap, LoadModel, LoadModelFromMesh, LoadTextureFromImage, SetConfigFlags, SetMaterialTexture, UnloadImage}, prelude::*
 };
 
 enum State {
@@ -14,6 +14,9 @@ enum State {
 }
 
 const FPS: u32 = 60;
+const NOISE_SIZE: ffi::Vector2 = ffi::Vector2{x: 256.0, y: 256.0};
+const MAP_SIZE: ffi::Vector2 = ffi::Vector2{x: 100.0, y: 100.0};
+const MAP_SCALE: f32 = MAP_SIZE.x*0.1;
 
 fn main() {
     unsafe {
@@ -26,11 +29,15 @@ fn main() {
         .build();
     
     let mut camera= Camera3D::perspective(
-        Vector3 { x: 0.0, y: 5.0, z: 5.0 }, 
+        Vector3 { x: 5.0, y: 5.0, z: 5.0 }, 
         Vector3 { x: 0.0, y: 0.0, z: 0.0 },
-        Vector3 {x: 0.0, y: 1.0, z: 0.0},
+        Vector3 {x: 0.0, y: 5.0, z: 0.0},
         45.0
     );
+    let perlin = Perlin::new(1);
+    let val = perlin.get([0.1, 0.0, 0.0]);
+    println!("{}", val);
+
     rl.set_target_fps(FPS);
     let (x, y, z) = (5.0, 5.0, 5.0);
     let mut mesh: ffi::Mesh;
@@ -42,6 +49,16 @@ fn main() {
     let mut pyramid: ffi::Model;
     let pyramid_path = CString::new("./src/Pyramid.glb").expect("cstr failed");
     let pyramid_ptr: *const i8 = pyramid_path.as_ptr();
+    let noise_image: ffi::Image;
+    let noise_texture: ffi::Texture2D;
+    let mut terrain_mesh: ffi::Mesh;
+    let mut terrain_model: ffi::Model;
+    let mut terrain_material: ffi::Material;
+    let mut terrain_material_map: ffi::MaterialMap;
+    let mut new_terrain_mesh: *mut ffi::Mesh;
+    let mut new_terrain_model: ffi::Model;
+    let mut new_terrain_material: *mut ffi::Material;
+    let mut new_terrain_material_map: *mut ffi::MaterialMap;
     unsafe {
         mesh = GenMeshCube(1.0, 1.0, 1.0);
         cylinder_mesh = GenMeshCylinder(1.0, 2.0, 100);
@@ -50,9 +67,19 @@ fn main() {
         cylinder_image = GenImageGradientLinear(20, 20, 1, ffi::ColorFromHSV(0.2, 0.7, 1.0), ffi::ColorFromHSV(0.5, 1.0, 1.0));
         cylinder_texture = LoadTextureFromImage(cylinder_image);
         pyramid = LoadModel(pyramid_ptr);
+        noise_image = GenImagePerlinNoise(NOISE_SIZE.x as i32, NOISE_SIZE.y as i32, 0, 0, MAP_SCALE);
+        noise_texture = LoadTextureFromImage(noise_image);
+        terrain_mesh = GenMeshHeightmap(noise_image, ffi::Vector3{ x: MAP_SIZE.x, y: 8.0, z: MAP_SIZE.y });
+        terrain_model = LoadModelFromMesh(terrain_mesh);
         SetMaterialTexture(cylinder_model.materials.wrapping_add(0), MATERIAL_MAP_ALBEDO as i32, cylinder_texture);
+        terrain_material = *terrain_model.materials.wrapping_add(0);
+        terrain_material_map = *terrain_material.maps.wrapping_add(0);
+        terrain_material_map.texture = noise_texture;
+        new_terrain_model = terrain_model;
+        (*(*terrain_model.materials.add(0)).maps.wrapping_add(MATERIAL_MAP_ALBEDO as usize)).texture = noise_texture;
+        UnloadImage(noise_image);
     }
-    let mut position: ffi::Vector3 = ffi::Vector3 {x: 0.0, y: 0.0, z: 0.0};
+    let mut terrain_position: ffi::Vector3 = ffi::Vector3 {x: -MAP_SIZE.x, y: 0.0, z: -MAP_SIZE.y};
     while !rl.window_should_close() {
         let mut dt = rl.get_frame_time();
         let mut d = rl.begin_drawing(&thread);
@@ -74,9 +101,9 @@ fn main() {
 
         {
             let mut d3= d.begin_mode3D(camera);
-            d3.draw_grid(10, 1.0);
+            d3.draw_grid(100, 1.0);
             unsafe {
-                DrawModel(pyramid, position, 1.0, raylib::ffi::ColorFromHSV(1.0, 1.0, 1.0));
+                DrawModel(terrain_model, terrain_position, 1.0, raylib::ffi::ColorFromHSV(130.0, 1.0, 1.0));
             }
             // d3.draw_line_3D(Vector3{x: -4.0, y: 0.0, z: -2.0}, Vector3{x: 5.0, y: 2.0, z: 3.0}, Color::LIME);
         }
